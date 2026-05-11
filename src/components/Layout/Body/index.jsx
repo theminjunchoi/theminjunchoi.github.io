d993650d2667638ee60badbf75cfa1611871fb03
MinJun Choi <mj043000@naver.com>
2026-05-11 17:21:38 +0900

    style: 좌우 패딩값 조절

--- src/components/Layout/Body/index.jsx
+++ src/components/Layout/Body/index.jsx
@@ -5,10 +5,15 @@ const BodyWrapper = styled.div`
   margin: 0 auto;
   padding-top: 80px;
   max-width: 900px;
-  padding-left: 24px;
-  padding-right: 24px;
+  padding-left: 16px;
+  padding-right: 16px;
   box-sizing: border-box;
   min-height: calc(100vh - 120px);
+
+  @media (min-width: 576px) {
+    padding-left: 24px;
+    padding-right: 24px;
+  }
 `
 
 const Body = ({ children }) => {
